b39882fa19efca7a728a2015451d0b1c1cc8c962
okawaramikan <okawaramikan@gmail.com>
2020-12-29 06:52:01 +0900

    git push origin main

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->null()->comment('ユーザ名');
-            $table->string('icon_image')->nullable()->comment('プロフィール画像');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
